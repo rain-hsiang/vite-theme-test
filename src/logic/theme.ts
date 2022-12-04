943b1cd1218b7d53b00141c52cfa94da1fe0b772
cat <50940061+rain-hsiang@users.noreply.github.com>
2022-12-04 16:49:08 +0800

    不知道为啥上面这种写法，本地为空的时候会报错内存溢出，之前没懂什么意思，现在ok了
    不知道为啥上面这种写法，本地为空的时候会报错内存溢出，之前没懂什么意思，现在ok了

--- src/logic/theme.ts
+++ src/logic/theme.ts
@@ -5,9 +5,11 @@ import { ref, watchEffect } from 'vue'
 const primary = "#e72528"
 
 export function useTheme() {
-  const theme = ref(localStorage.getItem('theme-schema') || primary)
-  useStorage('theme-schema', theme)
-
+  // const theme = ref(localStorage.getItem('theme-schema') || primary)
+  // useStorage('theme-schema', theme)
+  //不知道为啥上面这种写法，本地为空的时候会报错内存溢出，之前没懂什么意思，现在ok了
+  const theme = useStorage('theme-schema', primary)
+  
   watchEffect(() => {
     changeTheme(theme.value)
   });
